681ebe2180ce68bc1a5dc6dee8ce329379bf6d15
Onur Gündüz <ogunduz@gmail.com>
2026-02-09 04:12:25 +0100

    Use stack allocated varint buffer

--- writer.go
+++ writer.go
@@ -18,59 +18,29 @@ func NewWriter(wd *bufio.Writer) *Writer {
 
 // WriteMessage writes a variable number of messages to w.
 func (w *Writer) WriteMessage(messages ...*Message) error {
-	var err error
-	if len(messages) == 1 {
-		i := messages[0]
-		_, err = w.wd.Write(send(i.ID, i.Channel, i.Data))
-	} else {
-		_, err = w.wd.Write(sendBatch(messages))
-	}
-	if err != nil {
-		return err
-	}
+	var vb [10]byte // max 10 bytes per varint
 
-	if err := w.wd.Flush(); err != nil {
-		return err
-	}
+	for _, m := range messages {
+		header := uint64(m.ID<<4) | uint64(m.Channel)
+		bodyLen := uint64(encodingLength(header) + len(m.Data))
 
-	return nil
-}
+		// write body length varint
+		n := binary.PutUvarint(vb[:], bodyLen)
+		if _, err := w.wd.Write(vb[:n]); err != nil {
+			return err
+		}
 
-func send(id int, ch rune, data []byte) []byte {
-	header := uint64(id)<<4 | uint64(ch)
-	length := len(data) + encodingLength(header)
-	payload := make([]byte, encodingLength(uint64(length))+length)
+		// write header varint
+		n = binary.PutUvarint(vb[:], header)
+		if _, err := w.wd.Write(vb[:n]); err != nil {
+			return err
+		}
 
-	n := binary.PutUvarint(payload, uint64(length))
-	n += binary.PutUvarint(payload[n:], header)
-	copy(payload[n:], data)
-
-	return payload
-}
-
-func sendBatch(items []*Message) []byte {
-	offset := 0
-
-	var length int
-
-	for _, v := range items {
-		// 16 is >= the max size of the varints
-		length += 16 + encodingLength(uint64(len(v.Data)))
+		// write data payload
+		if _, err := w.wd.Write(m.Data); err != nil {
+			return err
+		}
 	}
 
-	payload := make([]byte, length)
-
-	for _, v := range items {
-		header := uint64(v.ID<<4) | uint64(v.Channel)
-		l := uint64(len(v.Data) + encodingLength(header))
-
-		offset += binary.PutUvarint(payload[offset:], l)
-		offset += binary.PutUvarint(payload[offset:], header)
-
-		copy(payload[offset:], v.Data)
-
-		offset += len(v.Data)
-	}
-
-	return payload[0:offset]
+	return w.wd.Flush()
 }
